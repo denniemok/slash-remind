a8c2820d4700f788d0cc44dc8c877ff27c932fab
Dennie Mok <mokdennie26@gmail.com>
2023-08-31 09:48:24 +1000

    Added util

---
+++ src/com/battleasya/slashremind/handler/Util.java
@@ -0,0 +1,17 @@
+package com.battleasya.slashremind.handler;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class Util {
+
+    public static void msgPlayer(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+    public static void broadcast(String message) {
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', message));
+    }
+
+}
